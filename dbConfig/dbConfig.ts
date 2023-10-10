3ca04b4f666faca8f6f6857df5703dd43751891b
TheCharismaticEnigma <avishkar2002@mail.com>
2023-10-10 18:31:08 +0530

    Setup MongoDB connection

---
+++ dbConfig/dbConfig.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+export async function connectToDatabase() {
+  try {
+    //   Without connect, no DB connection.
+    mongoose.connect(process.env.MONGO_URI!);
+    const connection = mongoose.connection;
+
+    connection.on('connected', () => {
+      console.log('CONNECTION WITH DATABASE ESTABLISHED');
+    });
+
+    connection.on('error', () => {
+      console.log('CONNECTION WITH DATABASE FAILED');
+      process.exit();
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
