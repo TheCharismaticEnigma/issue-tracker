a1d4ea378695792627c5497a8acd9815f00a80c6
TheCharismaticEnigma <avishkar2002@mail.com>
2023-10-11 12:43:41 +0530

    Setup Radix UI Library

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@radix-ui/themes';
+
 const IssuesPage = () => {
-  return <div className="text-white"> Hakuna Matata </div>;
+  return (
+    <div className="text-white">
+      {' '}
+      <Button>Hakuna Matata</Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
