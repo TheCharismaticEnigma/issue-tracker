801f08510319ddd87d5003268db218914487c32f
TheCharismaticEnigma <avishkar2002@mail.com>
2023-10-16 10:46:02 +0530

    Create Alert Dialogue Box for deleting issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,13 +1,43 @@
+'use client';
+
 import { TrashIcon } from '@radix-ui/react-icons';
-import { Button } from '@radix-ui/themes';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import Link from 'next/link';
 
 const DeleteIssueButton = ({ id }: { id: string }) => {
   return (
-    <Button color="red" className="max-w-xs">
-      <TrashIcon />
-      <Link href={`/issues/${id}/`}>Delete Issue </Link>
-    </Button>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red" className="max-w-xs">
+            <TrashIcon />
+            {/* <Link href={`/issues/${id}/`}>Delete Issue </Link> */}
+            Delete Issue
+          </Button>
+        </AlertDialog.Trigger>
+
+        <AlertDialog.Content size={'4'}>
+          <AlertDialog.Title size={'6'}>Delete Issue</AlertDialog.Title>
+          <AlertDialog.Description size="5">
+            Are you sure you want to delete this issue? This action can not be
+            undone.
+          </AlertDialog.Description>
+
+          <Flex gap="3" mt="5" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red">
+                Delete
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
