2bbc32f6f64b182ad90eb3c8cc0b9edcc7e2bd20
TheCharismaticEnigma <avishkar2002@mail.com>
2023-10-16 19:03:48 +0530

    Improve the User Experience

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,25 +1,35 @@
 'use client';
 
+import { Spinner } from '@/components';
 import { TrashIcon } from '@radix-ui/react-icons';
 import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import axios from 'axios';
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const DeleteIssueButton = ({ id }: { id: string }) => {
   const router = useRouter();
 
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [error, setError] = useState(false);
+
   const deleteIssue = async () => {
-    await axios.delete(`/api/issues/${id}`);
-    router.push('/issues');
-    router.refresh(); // to refresh the browser cache.
+    try {
+      setIsDeleting(true);
+      await axios.delete(`/api/issues/${id}`);
+      router.push('/issues');
+      router.refresh(); // to refresh the browser cache.
+    } catch (error) {
+      setError(true);
+      setIsDeleting(false);
+    }
   };
 
   return (
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red" className="max-w-xs">
+          <Button color="red" className="max-w-xs" disabled={isDeleting}>
             <TrashIcon />
             {/* <Link href={`/issues/${id}/`}>Delete Issue </Link> */}
             Delete Issue
@@ -45,6 +55,25 @@ const DeleteIssueButton = ({ id }: { id: string }) => {
               </Button>
             </AlertDialog.Action>
           </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content size={'4'}>
+          <AlertDialog.Title size={'6'}>ERROR!</AlertDialog.Title>
+          <AlertDialog.Description size="5" mb={'5'}>
+            Cannot delete issue. Please try again later.
+          </AlertDialog.Description>
+
+          <Button
+            variant="solid"
+            color="red"
+            onClick={() => {
+              setError(false);
+            }}
+          >
+            Proceed
+          </Button>
         </AlertDialog.Content>
       </AlertDialog.Root>
     </>
