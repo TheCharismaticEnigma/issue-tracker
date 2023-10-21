25433cc9ca92a2362c77a8bc32931952a3170338
TheCharismaticEnigma <avishkar2002@mail.com>
2023-10-21 16:48:56 +0530

    Setting up React Query in the app

---
+++ providers/QueryClientProvider.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+// Context and Providers are available in client components only.
+
+import {
+  QueryClient,
+  QueryClientProvider as ReactQueryClientProvider,
+} from '@tanstack/react-query';
+import { PropsWithChildren } from 'react';
+
+const queryClient = new QueryClient();
+
+const QueryClientProvider = ({ children }: PropsWithChildren) => {
+  return (
+    <ReactQueryClientProvider client={queryClient}>
+      {children}
+    </ReactQueryClientProvider>
+  );
+};
+
+export default QueryClientProvider;
